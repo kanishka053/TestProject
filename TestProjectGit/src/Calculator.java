5dfd9e34eba23cb5d6e67a9b903d02e5abf58b52
kanishka gupta <kanishkagupta0521@gmail.com>
2023-07-09 19:37:11 +0530

    SECOND COMMIT

---
+++ TestProjectGit/src/Calculator.java
@@ -0,0 +1,13 @@
+
+public class Calculator {
+	int n1=5,n2=10;
+	
+	public void add()
+	{
+		System.out.println("Sum is"+(n1+n2));
+	}
+	public static void main(String args[])
+	{
+		Calculator calculator=new Calculator();
+	}
+}
